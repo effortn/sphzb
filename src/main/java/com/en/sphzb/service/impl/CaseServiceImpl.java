9e6de1e36d28f1c0e73431c90be2a5d6386b0f6a
effortn <effortn@outlook.com>
2019-02-25 09:36:32 +0800

    案件相关Service新建提交

---
+++ src/main/java/com/en/sphzb/service/impl/CaseServiceImpl.java
@@ -0,0 +1,17 @@
+package com.en.sphzb.service.impl;
+
+import com.en.sphzb.VO.ResultVO;
+import com.en.sphzb.service.CaseService;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 案件Service实现类
+ * create by en
+ * at 2019/2/25 9:35
+ **/
+public class CaseServiceImpl implements CaseService {
+    @Override
+    public ResultVO uploadCases(MultipartFile file) {
+        return null;
+    }
+}
