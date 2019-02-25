14a59492fbdddc70ba19ae306f74d5a3492170af
”emerson <1732846832@qq.com>
2019-02-25 14:54:49 +0800

    案件上传

--- src/main/java/com/en/sphzb/controller/CaseController.java
+++ src/main/java/com/en/sphzb/controller/CaseController.java
@@ -1,11 +1,15 @@
 package com.en.sphzb.controller;
 
 import com.en.sphzb.VO.ResultVO;
+import com.en.sphzb.service.CaseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  * 案件相关控制器
@@ -15,7 +19,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping(value = "/case/")
 public class CaseController {
-
+    @Autowired
+    private CaseService caseService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index() {
@@ -25,8 +30,21 @@ public class CaseController {
     @ResponseBody
     @RequestMapping(value = "upload", method = RequestMethod.POST)
     public ResultVO caseUpload(MultipartFile cases) {
-        // TODO
+        try {
+            if(!cases.isEmpty()){
+                caseService.uploadCases(cases);
+            }else {
+                ResultVO resultVO = new ResultVO(1,"文件不存在");
+                return resultVO;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
         return null;
     }
+
 
 }
